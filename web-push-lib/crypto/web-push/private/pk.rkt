d4bd8bc9bd8cbc53374261fb57c510c6a425a4b5
Bogdan Popa <bogdan@defn.io>
2026-02-06 10:27:18 +0200

    pk: add notes re. formats

--- web-push-lib/crypto/web-push/private/pk.rkt
+++ web-push-lib/crypto/web-push/private/pk.rkt
@@ -5,9 +5,9 @@
 
 (provide
  generate-ecdh-private-key
+ encode-ecdh-private-key
  decode-ecdh-private-key
- decode-ecdh-public-key
- encode-ecdh-private-key)
+ decode-ecdh-public-key)
 
 ;; XXX: It would be nice if crypto-lib made curve-alias->oid public.
 (define prime256v1-curve-oid
@@ -18,17 +18,8 @@
    (get-pk 'ec factories)
    '((curve "prime256v1"))))
 
-(define (decode-ecdh-private-key public-bs private-bs [factories (crypto-factories)])
-  (define x
-    (for/fold ([n 0])
-              ([b (in-bytes private-bs)])
-      (bitwise-ior (arithmetic-shift n 8) b)))
-  (define spec `(ec private ,prime256v1-curve-oid ,public-bs ,x))
-  (datum->pk-key spec 'rkt-private factories))
-
-(define (decode-ecdh-public-key bs [factories (crypto-factories)])
-  (datum->pk-key `(ec public ,prime256v1-curve-oid ,bs) 'rkt-public factories))
-
+;; Returns the raw, uncompressed public key (ANSI X9.62) and the private
+;; key encoded as bytes in big endian order.
 (define (encode-ecdh-private-key pk)
   (match-define `(ec private ,_ ,public-bs ,x)
     (pk-key->datum pk 'rkt-private))
@@ -41,3 +32,16 @@
        (arithmetic-shift n -8)
        (cons (bitwise-and n #xFF) bs))))
   (values public-bs private-bs))
+
+;; The reverse of encode-ecdh-private-key.
+(define (decode-ecdh-private-key public-bs private-bs [factories (crypto-factories)])
+  (define x
+    (for/fold ([n 0])
+              ([b (in-bytes private-bs)])
+      (bitwise-ior (arithmetic-shift n 8) b)))
+  (define spec `(ec private ,prime256v1-curve-oid ,public-bs ,x))
+  (datum->pk-key spec 'rkt-private factories))
+
+;; The reverse of encode-ecdh-private-key, but for a public key only.
+(define (decode-ecdh-public-key bs [factories (crypto-factories)])
+  (datum->pk-key `(ec public ,prime256v1-curve-oid ,bs) 'rkt-public factories))
